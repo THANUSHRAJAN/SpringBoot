dc485a773a0ba9633bd9b1b716d0b8cd9bde17b4
THANUSH RAJAN A <113244134+THANUSHRAJAN@users.noreply.github.com>
2023-12-14 16:47:03 +0530

    Add files via upload

---
+++ bookStore/bookStore/src/main/java/com/bookStore/controller/BookController.java
@@ -0,0 +1,44 @@
+package com.bookStore.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.bookStore.entity.Book;
+import com.bookStore.service.BookService;
+import java.util.*;
+
+@Controller
+public class BookController {
+	
+	@Autowired
+	private BookService service;
+	
+	
+	
+	@GetMapping("/abc")
+	public String home() {
+		return "home";
+	}
+	
+	@GetMapping("/book_register")
+	public String bookRegister() {
+		return "bookRegister";
+	}
+	
+	@GetMapping("/available_books")
+	public ModelAndView getAllBook() {
+		List<Book>list=service.getAllBook();
+		return new ModelAndView("bookList","book",list);
+	}
+	
+	@PostMapping("/save")
+	public String addBook(@ModelAttribute Book b) {
+		service.save(b);
+		return "redirect:/available_books";
+	}
+	
+	
+}

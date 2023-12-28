2818cacea26b7f87620a8141c3d73806221c5b52
THANUSH RAJAN A <113244134+THANUSHRAJAN@users.noreply.github.com>
2023-12-28 16:49:08 +0530

    Add files via upload

--- bookStore/bookStore/src/main/java/com/bookStore/entity/Book.java
+++ bookStore/bookStore/src/main/java/com/bookStore/entity/Book.java
@@ -1,39 +1,59 @@
-package com.bookStore.entity;
+package com.Project.Book.model;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
 
 @Entity
-public class Book {
-	
+public class Bookshop {
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
-	private int id;
-	private String name;
 	
-	public Book(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
-		
-	}
-	public Book() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
-	public int getId() {
-		return id;
-	}
-	public void setId(int id) {
-		this.id = id;
-	}
-	public String getName() {
-		return name;
-	}
-	public void setName(String name) {
-		this.name = name;
-	}
-	
+	int id;
+	String name;
+	String phone;
+	String email;
+	String message;
+	 	public Bookshop(int id,String name,String phone,String email,String message) {
+	 		this.id=id;
+	 		this.name=name;
+	 		this.phone=phone;
+	 		this.email=email;
+	 		this.message=message;
+	 	}
+	 	public Bookshop() {
+	 		super();
+	 	}
+	 	
+		public int getId() {
+			return id;
+		}
+		public void setId(int id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+		public String getMessage() {
+			return message;
+		}
+		public void setMessage(String message) {
+			this.message = message;
+		}
+	 	
+
 }
+
